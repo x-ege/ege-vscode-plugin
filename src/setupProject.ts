7afc3ecbf359143eb208ca3e8e92874e3d5906c3
wysaid <admin@wysaid.org>
2025-07-17 11:30:10 +0800

    重命名模板cpp入口文件 demo.cpp => main.cpp, 重构 cmake 模板结构, 支持 ege 源码模式接入

--- src/setupProject.ts
+++ src/setupProject.ts
@@ -9,7 +9,7 @@ import * as path from 'path';
 import { ege } from './ege';
 import { copyDirRecursiveIfNotExist, copyIfNotExist } from './utils';
 
-export function setupProject(internalTemplatePath: string = "project_with_libs") {
+export function setupProject(usingSource?: boolean) {
     const workspaceFolders = vscode.workspace.workspaceFolders;
 
     for (const workspaceFolder of workspaceFolders || []) {
@@ -19,11 +19,11 @@ export function setupProject(internalTemplatePath: string = "project_with_libs")
         if (fs.existsSync(cmakeListsPath.fsPath)) {
             ege.printInfo("CMakeLists.txt 已存在!");
         } else {
-            const cmakeListsTemplatePath = path.join(__dirname, `../cmake_template/${internalTemplatePath}/CMakeLists.txt`);
+            const cmakeListsTemplatePath = path.join(__dirname, `../cmake_template/${usingSource ? "CMakeLists_src.txt" : "CMakeLists_lib.txt"}`);
             fs.copyFileSync(cmakeListsTemplatePath, cmakeListsPath.fsPath);
             ege.printInfo("CMakeLists.txt 已创建!");
 
-            /// 搜索一下项目目录下是否有别的 cpp 文件, 如果没有, 则拷贝 demo.cpp
+            /// 搜索一下项目目录下是否有别的 cpp 文件, 如果没有, 则拷贝 main.cpp
 
             const files = fs.readdirSync(workspaceDir, { encoding: "utf-8" });
             let hasCppFile = false;
@@ -35,15 +35,15 @@ export function setupProject(internalTemplatePath: string = "project_with_libs")
             }
 
             if (!hasCppFile) {
-                /// 拷贝 demo.cpp
-                copyIfNotExist(path.join(__dirname, `../cmake_template/${internalTemplatePath}/demo.cpp`), `${workspaceDir}/demo.cpp`);
+                /// 拷贝 main.cpp
+                copyIfNotExist(path.join(__dirname, `../cmake_template/main.cpp`), `${workspaceDir}/main.cpp`);
             } else {
-                ege.printInfo("项目目录下已存在 cpp 文件, 跳过创建 demo.cpp!");
+                ege.printInfo("项目目录下已存在 cpp 文件, 跳过创建 main.cpp!");
             }
         }
 
-        // 递归拷贝(不覆盖) cmake_template/project_with_libs/.vscode 目录下的所有内容到工作区根目录
-        copyDirRecursiveIfNotExist(path.join(__dirname, `../cmake_template/${internalTemplatePath}/.vscode`), `${workspaceDir}/.vscode`);
+        // 递归拷贝(不覆盖) cmake_template/.vscode 目录下的所有内容到工作区根目录
+        copyDirRecursiveIfNotExist(path.join(__dirname, `../cmake_template/.vscode`), `${workspaceDir}/.vscode`);
 
         /// 如果根目录没有 ege 目录, 拷贝一个过去
         const egeDir = `${workspaceDir}/ege`;
@@ -52,9 +52,15 @@ export function setupProject(internalTemplatePath: string = "project_with_libs")
             fs.mkdirpSync(egeDir);
             ege.printInfo("ege 目录已创建!");
 
-            /// 拷贝整个 ege 目录
-            const egeSrcDir = path.join(__dirname, "../bundle/ege_bundle");
-            fs.copySync(egeSrcDir, egeDir);
+            if (usingSource) {
+                const egeSrcDir = path.join(__dirname, "../bundle/ege_src");
+                fs.copySync(egeSrcDir, egeDir);
+            } else {
+                /// 拷贝整个 ege 目录
+                const egeLibDir = path.join(__dirname, "../bundle/ege_bundle");
+                fs.copySync(egeLibDir, egeDir);
+            }
+
         } else {
             ege.printInfo("ege 目录已存在, 跳过创建!");
         }
